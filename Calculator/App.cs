a5803bd390f632692ed8b17d82a063580c749491
Sebastian <sebas14121999@gmail.com>
2019-10-28 15:49:19 -0500

    Agregar archivos de proyecto.

---
+++ Calculator/App.cs
@@ -0,0 +1,38 @@
+﻿using Calculator.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace Calculator
+{
+	public class App : Application
+	{
+		public App()
+		{
+            // The root page of your application
+            //MainPage = new MainPage();
+            MainPage = new Page1();
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ Calculator/ViewModels/OperacionesViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Calculator.ViewModels
+{
+    class OperacionesViewModel : ViewModelBase
+    {
+        string inputString = "";
+        string displayText = "";
+        char[] specialChars = { '*', '#' };
+
+        
+        // Constructor
+        public OperacionesViewModel()
+        {
+            SelectNumber = new Command<string>((key) =>
+            {
+                InputString += key;
+            });
+
+            Delete = new Command(() =>
+            {
+                InputString = InputString.Substring(0, InputString.Length - 1);
+            },
+                () =>
+                {
+                    return InputString.Length > 0;
+                });
+        }
+
+        public string InputString
+        {
+            protected set
+            {
+                if (inputString != value)
+                {
+                    inputString = value;
+                    OnPropertyChanged();
+                    DisplayText = inputString;
+
+                    ((Command)Delete).ChangeCanExecute();
+                }
+            }
+
+            get { return inputString; }
+        }
+
+        public string DisplayText
+        {
+            protected set
+            {
+                if (displayText != value)
+                {
+                    displayText = value;
+                    OnPropertyChanged();
+                }
+            }
+            get { return displayText; }
+        }
+
+        // ICommand
+        public ICommand SelectNumber { protected set; get; }
+
+        public ICommand Delete { protected set; get; }
+
+    }
+}
